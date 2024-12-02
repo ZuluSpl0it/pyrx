0f0b17ed505268738c6c9717dfff2ae88603c1d1
ZuluSpl0it <55359362+ZuluSpl0it@users.noreply.github.com>
2024-12-01 21:39:30 -0600

    Update rx-slow-hash.c

--- src/rx-slow-hash.c
+++ src/rx-slow-hash.c
@@ -226,7 +226,7 @@ void rx_slow_hash(const uint64_t mainheight, const uint64_t seedheight, const ch
     if (cache == NULL) {
       cache = randomx_alloc_cache(flags | RANDOMX_FLAG_LARGE_PAGES);
       if (cache == NULL) {
-        mdebug(RX_LOGCAT, "Couldn't use largePages for RandomX cache");
+        /*mdebug(RX_LOGCAT, "Couldn't use largePages for RandomX cache");*/
         cache = randomx_alloc_cache(flags);
       }
       if (cache == NULL)
@@ -251,7 +251,7 @@ void rx_slow_hash(const uint64_t mainheight, const uint64_t seedheight, const ch
       if (rx_dataset == NULL) {
         rx_dataset = randomx_alloc_dataset(RANDOMX_FLAG_LARGE_PAGES);
         if (rx_dataset == NULL) {
-          mdebug(RX_LOGCAT, "Couldn't use largePages for RandomX dataset");
+          /*mdebug(RX_LOGCAT, "Couldn't use largePages for RandomX dataset");*/
           rx_dataset = randomx_alloc_dataset(RANDOMX_FLAG_DEFAULT);
         }
         if (rx_dataset != NULL)
@@ -267,7 +267,7 @@ void rx_slow_hash(const uint64_t mainheight, const uint64_t seedheight, const ch
     }
     rx_vm = randomx_create_vm(flags | RANDOMX_FLAG_LARGE_PAGES, rx_sp->rs_cache, rx_dataset);
     if(rx_vm == NULL) { //large pages failed
-      mdebug(RX_LOGCAT, "Couldn't use largePages for RandomX VM");
+      /*mdebug(RX_LOGCAT, "Couldn't use largePages for RandomX VM");*/
       rx_vm = randomx_create_vm(flags, rx_sp->rs_cache, rx_dataset);
     }
     if(rx_vm == NULL) {//fallback if everything fails
